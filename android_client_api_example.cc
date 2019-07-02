316b8d95103e50402a911476f91329d501f1ba25
Raymond Chiu <chiur@google.com>
2019-07-01 18:35:27 -0700

    Updated example and sdk
    - Added HeapBuffered to SDK
    - Added data source descriptor to example

--- android_client_api_example.cc
+++ android_client_api_example.cc
@@ -16,11 +16,18 @@
 
 //#include "perfetto/tracing.h"
 //
+//#include "perfetto/common/gpu_counter_descriptor.pbzero.h"
+//#include "perfetto/config/gpu/gpu_counter_config.pbzero.h"
+//#include "perfetto/protozero/scattered_heap_buffer.h"
+//#include "perfetto/trace/gpu/gpu_counter_event.pbzero.h"
+//#include "perfetto/trace/gpu/gpu_render_stage_event.pbzero.h"
 //#include "perfetto/trace/test_event.pbzero.h"
 //#include "perfetto/trace/trace.pb.h"
 //#include "perfetto/trace/trace_packet.pbzero.h"
-//#include "perfetto/config/gpu/gpu_counter_config.pbzero.h"
 #include "perfetto.h"
+
+#include <unordered_map>
+#include <vector>
 
 // Deliberately not pulling any non-public perfetto header to spot accidental
 // header public -> non-public dependency while building this file.
@@ -36,37 +43,80 @@ class MyDataSource : public perfetto::DataSource<MyDataSource> {
     for(auto it = config.counter_ids(); it; ++it) {
       counter_ids.push_back(it->as_uint32());
     }
+    first = true;
   }
 
   void OnStart(const StartArgs&) override { PERFETTO_ILOG("OnStart called"); }
 
   void OnStop(const StopArgs&) override { PERFETTO_ILOG("OnStop called"); }
 
+  bool first = true;
   std::vector<uint32_t> counter_ids;
 };
 
 PERFETTO_DEFINE_DATA_SOURCE_STATIC_MEMBERS(MyDataSource);
 
 int main() {
+  const std::unordered_map<uint32_t, const char*> COUNTER_MAP {
+    { 0, "foo" },
+    { 1, "bar" }
+  };
+
   perfetto::TracingInitArgs args;
   args.backends = perfetto::kSystemBackend;
   perfetto::Tracing::Initialize(args);
 
   // DataSourceDescriptor can be used to advertise domain-specific features.
-  perfetto::DataSourceDescriptor dsd;
-  dsd.set_name("com.example.mytrace");
-  MyDataSource::Register(dsd);
+  {
+    perfetto::DataSourceDescriptor dsd;
+    dsd.set_name("com.example.mytrace");
 
+    protozero::HeapBuffered<perfetto::protos::pbzero::GpuCounterDescriptor> proto;
+    for (auto it : COUNTER_MAP) {
+    auto spec = proto->add_specs();
+      spec->set_counter_id(it.first);
+      spec->set_name(it.second);
+    }
+    dsd.set_gpu_counter_descriptor_raw(proto.SerializeAsString());
+    MyDataSource::Register(dsd);
+  }
+
+  uint64_t i = 0;
   for (;;) {
-    MyDataSource::Trace([](MyDataSource::TraceContext ctx) {
+    MyDataSource::Trace([&](MyDataSource::TraceContext ctx) {
       PERFETTO_LOG("Tracing lambda called");
       auto data_source = ctx.GetDataSourceLocked();
-      for(auto it : data_source->counter_ids) {
-        PERFETTO_LOG("counter id: %d", it);
+      if (data_source->first) {
+        i = 0;
+        auto packet = ctx.NewTracePacket();
+        packet->set_timestamp(0);
+        auto counter_event = packet->set_gpu_counter_event();
+        auto desc = counter_event->set_counter_descriptor();
+        for (auto it : data_source->counter_ids) {
+          auto entry = COUNTER_MAP.find(it);
+          if (entry != COUNTER_MAP.end()) {
+            auto spec = desc->add_specs();
+            spec->set_counter_id(entry->first);
+            spec->set_name(entry->second);
+          }
+        }
+        packet->Finalize();
+        data_source->first = false;
       }
-      auto packet = ctx.NewTracePacket();
-      packet->set_timestamp(42);
-      packet->set_for_testing()->set_str("event 1");
+      i++;
+      {
+        auto packet = ctx.NewTracePacket();
+        packet->set_timestamp(i * 10);
+        auto counter_event = packet->set_gpu_counter_event();
+        auto counters = counter_event->add_counters();
+        counters->set_counter_id(i % 3);
+        if (i % 3 == 0) {
+          counters->set_double_value(static_cast<double>(i));
+        } else {
+          counters->set_int_value(static_cast<int64_t>(i));
+        }
+        packet->Finalize();
+      }
     });
     sleep(1);
   }
